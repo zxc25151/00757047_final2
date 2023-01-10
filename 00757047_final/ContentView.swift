2e3c83e3b0f23ca7a385d0797ab259f310211a97
allen <allenyeh929@gmail.com>
2023-01-11 00:49:52 +0800

    Initial Commit

---
+++ 00757047_final/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  00757047_final
+//
+//  Created by Jun3631 on 2023/1/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 00757047_final/_0757047_finalApp.swift
@@ -0,0 +1,17 @@
+//
+//  _0757047_finalApp.swift
+//  00757047_final
+//
+//  Created by Jun3631 on 2023/1/11.
+//
+
+import SwiftUI
+
+@main
+struct _0757047_finalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
